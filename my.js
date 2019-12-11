ee85c2c53d116a1ce3d1f401de2a51f7c553f8d5
LuisGarcia23 <44875219+LuisGarcia23@users.noreply.github.com>
2019-12-11 08:53:58 -0600

    Add files via upload

--- my.js
+++ my.js
@@ -6,16 +6,16 @@ function onDeviceReady() {
 var db = window.sqlitePlugin.openDatabase({name: "my.db"});
 show();
 db.transaction(function(tx) {
-tx.executeSql('CREATE TABLE IF NOT EXISTS mydata (id integer primary key, name text, email text)');
+tx.executeSql('CREATE TABLE IF NOT EXISTS mydata (id integer primary key, name text, nume integer)');
 });
 
 //<!--Method to insert new row in the database-->
 $(document).on('click', '#creat', function(){
 var name = $("#name").val();
-var email = $("#email").val();
+var num = $("#numero").val();
 db.transaction(function(transaction) {
-var executeQuery = "INSERT INTO mydata (name, email) VALUES (?,?)";
-transaction.executeSql(executeQuery, [name,email]
+var executeQuery = "INSERT INTO mydata (name, nume) VALUES (?,?)";
+transaction.executeSql(executeQuery, [name,num]
 , function(tx, result) {
 show();
 },
@@ -32,7 +32,7 @@ transaction.executeSql('SELECT * FROM mydata', [], function (tx, results) {
 var key = "";
 
 //<!--Display the table head-->
-var pair="<tr><th data-priority=\"1\"><center>Id</center></th><th data-priority=\"1\"><center>Name</center></th><th data-priority=\"2\"><center>Email</center></th><th><center>Update</center></th><th><center>Delete</center></th></tr>";
+var pair="<tr><th data-priority=\"1\"><center>Id</center></th><th data-priority=\"1\"><center>Name</center></th><th data-priority=\"2\"><center>Numero</center></th><th><center>Update</center></th><th><center>Delete</center></th></tr>";
 var i=0;
 
 //<!--results.rows.length to get the total number of rows stored in the database-->
@@ -46,9 +46,9 @@ key = results.rows.item(i).name;
 id = results.rows.item(i).id;
 
 //<!--Displaying all rows of the database in the table-->
-pair += "<tr><td><center>"+id+"</center></td><td><center>"+key+"</center></td><td><center>"+results.rows.item(i).email+"</center></td><td><a class=\"update\" href=\"#myPopupDialog\" data-custom="+"'"+ id+ "'" +"data-rel=\"popup\" data-position-to=\"window\" data-transition=\"pop\"><center><i class='fa fa-pencil-square-o'></i></center></a></td><td><a id=\"delete\" data=\""+id+"\"><center><i class='fa fa-trash'></i></center></a></td></tr>";
+pair += "<tr><td><center>"+id+"</center></td><td><center>"+key+"</center></td><td><center>"+results.rows.item(i).nume+"</center></td><td><a class=\"update\" href=\"#myPopupDialog\" data-custom="+"'"+ id+ "'" +"data-rel=\"popup\" data-position-to=\"window\" data-transition=\"pop\"><center><i class='fa fa-pencil-square-o'></i></center></a></td><td><a id=\"delete\" data=\""+id+"\"><center><i class='fa fa-trash'></i></center></a></td></tr>";
 }
-if (pair == "<tr><th>Name</th><th>Email</th></tr>") {
+if (pair == "<tr><th>Name</th><th>Numero</th></tr>") {
 pair += "<tr><td><i>empty</i></td><td><i>empty</i></td></tr>";
 }
 $("#myTable").html(pair);
@@ -74,26 +74,25 @@ function(error){
 $(document).on('click', '#upd', function(){
 var id = $("#id").val();
 var name = $("#uname").val();
-var email = $("#uemail").val();
+var num = $("#unumero").val();
 db.transaction(function(transaction) {
 var executeQuery = "";
-transaction.executeSql("UPDATE mydata SET name=?, email=? WHERE id=?", [name,email,id],
+transaction.executeSql("UPDATE mydata SET name=?, nume=? WHERE id=?", [name,num,id],
 function(tx, result) {alert('Updated successfully');
 show();
 },
 function(error){alert('Something went Wrong');});
 });
 });
-
 $(document).on('click', '.update', function(){
 var id = $(this).attr('data-custom');
 $("#id").val(id);
 db.transaction(function(transaction) {
-transaction.executeSql('SELECT name,email FROM mydata where id=?', [id], function (tx, results) {
+transaction.executeSql('SELECT name,nume FROM mydata where id=?', [id], function (tx, results) {
 var name = results.rows.item(0).name;
-var email = results.rows.item(0).email;
+var num = results.rows.item(0).nume;
 $("#uname").val(name);
-$("#uemail").val(email);
+$("#unumero").val(num);
 },
 function(error){
 alert('Something went Wrong');
